46832970342656052f41486540530ce6ddb1b40f
michelleroos <hello.michelleroos@gmail.com>
2021-04-06 12:15:41 -0400

    Setup Users & Sessions controllers

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
     validates :email, :session_token, uniqueness: true
     validates :password, length: {minimum: 6, allow_nil: true}
 
-    before_validations :ensure_session_token
+    after_initialize :ensure_session_token
 
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
