127173d5b01a76496c2c8c1694cd8609b733c69c
Collin Winner <cmwinner89@gmail.com>
2021-04-06 16:54:15 -0400

    Add bands table and model

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -9,6 +9,7 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
+
 class User < ApplicationRecord
 
     validates :email, :password_digest, :session_token, presence: true
